6ca76efc8cfaf5ef8734a864d801e164518a7601
GlaucoLeVif <victor.papleux@gmail.com>
2017-12-10 22:18:41 +0100

    test

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]){
     MyCanvas* SFMLView = new MyCanvas(MainFrame, QPoint(20, 20), QSize(360, 360));
     SFMLView->show();
     return App.exec();
+    tesgbdrhdrhdr
 }
 
 /*
