80dab61bc74f9b87aae4764d9f4b634b1d56a556
SimonKnu <anderlecht.11@hotmail.com>
2017-12-17 17:42:57 +0100

    g

--- sound.h
+++ sound.h
@@ -2,12 +2,12 @@
 #define SOUND_H
 
 #include <vector>
-//#include <SFML/Audio.hpp>
+#include <SFML/Audio.hpp>
 #include <string>
 
 class Sound
 {
-   /* private:
+    private:
 
         std::vector< sf::SoundBuffer> buffers;
         std::vector< sf::Sound> sounds;
@@ -19,7 +19,7 @@ class Sound
         static Sound* getInstance();
 
         void addFile(std::string txt);
-        void play(int index);*/
+        void play(int index);
 };
 
 #endif // SOUND_H
